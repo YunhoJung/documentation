4e1f306d6b76cea6265796661034e171d0408408
Jung YunHo <saijdu2198@gmail.com>
2017-06-05 16:21:51 -0700

    return topping_list into __str__ of Pizza_model

--- django_app/introduction_to_models/models/topping_and_pizza.py
+++ django_app/introduction_to_models/models/topping_and_pizza.py
@@ -16,7 +16,20 @@ class Pizza(models.Model):
     topping = models.ManyToManyField(Topping)
 
     def __str__(self):
-        return self.name
+        # 자신이 가지고 있는 토핑목록을 뒤에 출력
+        # ex) 치즈피자 (치즈, 토마토소스)
+        # topping_string = ''
+        # for topping in self.topping.all():
+        #     topping_string += topping.name
+        #     topping_string += ', '
+        #
+        # topping_string = topping_string[:-2]
+        # return '{} ({})'.format(self.name, topping_string)
+        # str.join, list comprehension을 사용해서 한 줄로 줄이기
+        return '{}의 토핑 : {}'.format(self.name, ', '.join(t.name for t in self.topping.all()))
+
 
     class Meta:
         ordering = ('name',)
+
+# M2M에서는 중간에 pk를 공유하는 테이블이 생겨서 공유
